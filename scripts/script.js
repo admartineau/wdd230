03f3045ec6a6d392cb327aec0bebb2a955b806fc
admartineau <mar13106@byui.edu>
2022-09-17 16:24:30 -0700

    Java914424p

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+year = new Date().getFullYear();
+document.querySelector("#year").textContent = year
+document.querySelector("#lastpageupdate").textContent= `Last Updated: ${document.lastModified}`;
