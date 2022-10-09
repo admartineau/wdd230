37ed8204763bf53c419af2acb98f15931086dfd2
admartineau <mar13106@byui.edu>
2022-10-08 23:03:30 -0600

    10081103p

--- lesson3/scripts/script.js
+++ lesson3/scripts/script.js
@@ -1,3 +1,11 @@
 year = new Date().getFullYear();
 document.querySelector("#year").innerHTML = year
 document.querySelector("#lastpageupdate").innerHTML= `Last Updated: ${document.lastModified}`;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
