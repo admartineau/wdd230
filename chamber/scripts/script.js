8c8b8c2e6a9ae8ed5667d8a4f27c50ca2b35f67a
admartineau <mar13106@byui.edu>
2022-10-08 22:00:58 -0600

    100810p

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,5 @@
+
+
 year = new Date().getFullYear();
 document.querySelector("#year").innerHTML = year
 document.querySelector("#lastpageupdate").innerHTML= `Last Updated: ${document.lastModified}`;
