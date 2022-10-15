2571f47c6ff3af387f3060ea06bc87bfe6081ef2
admartineau <mar13106@byui.edu>
2022-10-14 20:25:59 -0600

    1014825p

---
+++ dom_manipulation/scripts/script.js
@@ -0,0 +1,41 @@
+year = new Date().getFullYear();
+document.querySelector("#year").innerHTML = year
+document.querySelector("#lastpageupdate").innerHTML= `Last Updated: ${document.lastModified}`;
+
+const input = document.getElementById("favchap");
+const button = document.getElementById("button");
+const list = document.getElementById("scripture_list");
+
+function addChapter() {
+
+
+    // safeguard blank values
+    if (input.value != "") {
+        
+        // create the list item
+
+        const listItem = document.createElement('li');
+        listItem.innerText = input.value;
+
+        // create the delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = "<span role='img' aria-label='Red X - Delete'>❌</span>";
+
+        // append to list item
+        listItem.appendChild(deleteButton);
+
+        // append to list
+        list.appendChild(listItem);
+
+        // add listener
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(this.parentElement);
+        })
+
+    }
+    input.value = "";
+    input.focus();
+
+}
+
+button.addEventListener("click", addChapter);
