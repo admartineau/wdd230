cf509a9c35b792f88c0cf97ff2d2f34ca4fbd535
admartineau <mar13106@byui.edu>
2022-10-27 23:44:14 -0600

    10271144

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,11 +1,14 @@
+// last update in footer
 year = new Date().getFullYear();
 document.querySelector("#year").innerHTML = year
 document.querySelector("#lastpageupdate").innerHTML= `Last Updated: ${document.lastModified}`;
 
+// hamburger navigation toggle button
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
 }
-
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
+
+//
