fc12b024abf1166ab875c40254f60b22cedf9ac1
admartineau <mar13106@byui.edu>
2022-11-26 14:40:48 -0700

    1126240p

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,7 +1,7 @@
 // last update in footer
 year = new Date().getFullYear();
 document.querySelector("#year").innerHTML = year;
-document.querySelector("#lastpageupdate").innerHTML= `Last Updated: ${document.lastModified}`;
+document.querySelector("#lastpageupdate").innerHTML= `Last Modified: ${document.lastModified}`;
 
 // hamburger navigation toggle button
 function toggleMenu() {
@@ -55,24 +55,6 @@ if (URL == "join.html") {
     document.getElementById("application_date").value = date.getTime();
 }
 
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-
-//gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	//display.classList.add("grid");
-	//display.classList.remove("list");
-//});
-
-//listbutton.addEventListener("click", showList); // example using defined function
-
-//function showList() {
-	//display.classList.add("list");
-	//display.classList.remove("grid");
-//}
-
 // todays date 
 
 const datefield = document.querySelector(".date");
@@ -96,4 +78,62 @@ if (dayWeek == 1 | dayWeek ==2) {
         banner.style.display = "block";
         banner.innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
     }
+}
+
+// weather api
+
+let currentTemp = document.querySelector('#current-temp');
+let weatherIcon = document.querySelector('#weather-icon');
+let captionDesc = document.querySelector('#description');
+let windChill = document.querySelector('#windChill');
+let windSpeed = document.querySelector('#windSpeed');
+
+let id = 5429032;
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?';
+let apiKey = '4f6fd97aee4a892adb33594c0527da37';
+let units = 'imperial'
+//
+
+let apiURL = `${baseURL}id=${id}&units=${units}&appid=${apiKey}`
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(apiURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+            wind(data);
+        } else {
+            console.log(`Response not OK ${await response.text()}`);
+        }
+    } catch (error) {
+        console.log(`Error: ${error}`)
+    }
+}
+
+apiFetch()
+
+//captialize
+function captialize(string) {
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+}
+
+//wind
+function wind(data) {
+    const tempF = data.main.temp;
+    const speed = data.wind.speed;
+    return windchill = (35.74 + (0.6215*tempF) - 35.75*(Math.pow(speed, 0.16)) + (0.4275*tempF)*(Math.pow(speed, 0.16))).toFixed(2);
+}
+// display the results 
+function displayResults(data) {
+    currentTemp.textContent = data.main.temp;
+    windSpeed.textContent = data.wind.speed;
+    let desc = captialize(data.weather[0].description)
+    captionDesc.textContent = desc;
+    windChill.textContent = Math.round(wind(data) * 100) / 100;
+    let icon = data.weather[0].icon;
+    weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    weatherIcon.alt = `Littleton, Colorado Weather: ${desc}${icon}`;
 }
